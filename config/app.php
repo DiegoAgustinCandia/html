978243d69e63ca5208dd35ff454693482cd0be05
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-12 18:44:03 -0300

    HOLI

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+const APP_URL = "http://localhost/tienda/";
+const APP_NAME = "TIENDA CRUD";
+const APP_SESSION_NAME = "CRUD";
+
+date_default_timezone_set("America/Argentina/Buenos_Aires");
