e8097a703ce0068a4b7cf1b7cf45d9f782f37e66
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-12 21:47:41 -0300

    Modifique el index

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-require_once "./autoload.php";
+require_once "./config/app.php";
 require_once "./autoload.php";
 
-
+if(isset($_GET['views'])){
+    $url=explode("/",$_GET['views']);
+}else{
+    $url=["login"];
+}
 
 
 
